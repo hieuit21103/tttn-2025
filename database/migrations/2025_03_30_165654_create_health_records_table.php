9fb6a006e189ea46818b506587f6df7a1ba2df4a
Lê Minh Hiếu <hieuit21103@gmail.com>
2025-03-31 13:55:53 +0700

    Update 2025_03_30_165654_create_health_records_table.php

--- database/migrations/2025_03_30_165654_create_health_records_table.php
+++ database/migrations/2025_03_30_165654_create_health_records_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('health_records', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->date('date_of_birth');
+            $table->enum('blood_type', ['A', 'B', 'AB', 'O'])->nullable();
+            $table->boolean('has_chronic_disease')->default(false);
+            $table->text('chronic_disease_notes')->nullable();
+            $table->boolean('has_allergies')->default(false);
+            $table->text('allergies_notes')->nullable();
+            $table->text('medical_history')->nullable();
+            $table->text('emergency_contact_name');
+            $table->text('emergency_contact_relationship');
+            $table->string('emergency_contact_phone');
             $table->timestamps();
         });
     }
